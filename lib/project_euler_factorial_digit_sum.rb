42ace3e27a25f3023feb24895d9acffae33e67cc
Lief Friedrichs <lief.friedrichs@gmail.com>
2020-07-08 15:56:05 +0000

    Done.

--- lib/project_euler_factorial_digit_sum.rb
+++ lib/project_euler_factorial_digit_sum.rb
@@ -1,1 +1,17 @@
 # code your solution here
+def factorial(n)
+  output = n 
+  until n == 1 do 
+    n -= 1
+    output *= n
+  end
+  output
+end 
+
+def sum_of_digits(n)
+  n.digits.sum
+end 
+
+def factorial_digit_sum(n) 
+  sum_of_digits(factorial(n))
+end 
